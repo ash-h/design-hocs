015bd4f69928162da3b113ae699ad7d61e0ef479
ash-h <a.henry@kuumba.co.uk>
2021-09-03 08:51:13 +0100

    add timeline designs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,29 @@
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
+const systemNotes = document.querySelectorAll('.timeline ul li');
+
+const toggleSwitch = document.getElementsByClassName('note_toggle')[0];
+
+
+const checkToggle = () => {
+  if (toggleSwitch.checked) {
+    for (let x of systemNotes) {
+      if (x.classList.contains('system')) {
+        x.classList.add("govuk-visually-hidden")
+      }
+    }
+  } else {
+    for (let x of systemNotes) {
+      if (x.classList.contains('system')) {
+        x.classList.remove("govuk-visually-hidden")
+      }
+    }
+  }
+}
+toggleSwitch.addEventListener('click',()=> {
+  checkToggle()
+})
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
